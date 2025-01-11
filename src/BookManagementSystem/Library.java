5f9427705dc75258650b53cb5993236489d31d02
Prashant Kumar <akhilla76@gmail.com>
2025-01-11 13:56:58 +0530

    Task 1

---
+++ src/BookManagementSystem/Library.java
@@ -0,0 +1,24 @@
+package BookManagementSystem;
+
+public class Library{
+    public Book[] books;
+
+    public Library () {
+        this.books = new Book[5];
+    }
+
+    //method to add a book to the library
+    public void addBook(Book book) {
+
+    }
+
+    //method to replace a book from the library
+    public void replaceBook(int bookID) {
+
+    }
+
+    //method to display all the books in the library
+    public void displayBooks() {
+
+    }
+}
---
+++ src/BookManagementSystem/bookManagementSystem.java
@@ -0,0 +1,10 @@
+package BookManagementSystem;
+
+public class bookManagementSystem {
+    public static void main(String[] args) {
+
+        Library library = new Library();
+        //implement a menu-driven user interface to interact with the library system
+        //Allow users to add, replace and display books
+    }
+}
